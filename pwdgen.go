ede3ab38cbafdc1aefb5dd6cc17413a92dd987a3
xartreal <xartreal@users.noreply.github.com>
2015-12-03 20:15:15 +0300

    Added plen in args

--- pwdgen.go
+++ pwdgen.go
@@ -4,16 +4,27 @@ import (
 	"fmt"
 	"os"
         "time"
+	"strconv"
 
 	"github.com/Luzifer/password/lib"
 )
 
 var pwd *securepassword.SecurePassword
+var plen int
+var err error
 
 func main() {
+        if len(os.Args) < 2 {
+                plen=12
+        } else {
+			plen,err=strconv.Atoi(os.Args[1])
+			if err!=nil {
+				plen=12
+			}
+        }
         pwd = securepassword.NewSecurePassword()
         for i:=1;i<10;i++ {
-	password, err := pwd.GeneratePassword(12, false)
+	password, err := pwd.GeneratePassword(plen, false)
 	if err != nil {
 		switch {
 		case err == securepassword.ErrLengthTooLow:
